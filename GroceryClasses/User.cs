262770f2c0568f52692a7a5576318652bbcf94de
conclancy <conclancy@hotmail.com>
2020-11-19 21:26:38 -0500

    Added "user" class.

---
+++ GroceryClasses/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryClasses
+{
+    public class User
+    {
+        public string username { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public IDictionary<Item, int> groceryList { get; set; }
+
+        public User(string username, string firstName, string lastName, string email, string phoneNumber, Dictionary<Item, int> groceryList)
+        {
+            this.username = username;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+            this.groceryList = groceryList;
+        }
+    }
+
+    
+}
--- GroceryListConsoleApp/Program.cs
+++ GroceryListConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using GroceryClasses;
 
 namespace GroceryListConsoleApp
@@ -6,14 +8,26 @@ namespace GroceryListConsoleApp
     class Program
     {
         static Item singleBanana = new Item("4012", "banana", 1.0, "each");
-        static Item bananas = new Item("4012", "bunch of bananas",
-            1.6, "lbs");
+        static Item bananas = new Item("4012", "bunch of bananas", 1.6, "lbs");
+        //static User Connor = new User("conclancy", "Connor", "Clancy", "conclancy@hotmail.com","4253017745",)
 
         static void Main(string[] args)
         {
-            Console.WriteLine($"{singleBanana.upc}, {singleBanana.size}");
-            Console.WriteLine($"{bananas.description}, {bananas.size}");
-            Console.WriteLine("Done");
+            var glist = new Dictionary<Item, int>
+                {
+                    {singleBanana, 1 },
+                    {bananas, 2 }
+                };
+
+            var connor = new User("conclancy", "Connor", "Clancy", "conclancy@hotmail.com", "4253017745", glist);
+
+            Console.WriteLine($"{connor.username} ordered:");
+
+            foreach (KeyValuePair<Item, int> kvp in glist)
+            {
+                var desc = kvp.Key;
+                Console.WriteLine($"Item Description {desc.description}, Qty: {kvp.Value}");
+            }
         }
     }
 }
